f943ca1e2bbc5c39b515240214573356f33ca55c
Ayleen Weiss <ayweit00@hs-esslingen.de>
2023-09-15 17:59:23 +0200

    Refine UI

--- app/src/main/java/de/naemiluna/temperatureconverter_android/MainActivity.kt
+++ app/src/main/java/de/naemiluna/temperatureconverter_android/MainActivity.kt
@@ -3,7 +3,6 @@ package de.naemiluna.temperatureconverter_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
@@ -16,7 +15,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -46,12 +44,10 @@ fun ConverterView() {
     var inputText by remember { mutableStateOf("") }
     var outputText by remember { mutableStateOf("") }
 
-    // Create a Column layout
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         // Input field for numbers
@@ -60,13 +56,11 @@ fun ConverterView() {
             onValueChange = {
                 inputText = it
             },
-            label = { Text("Enter numbers") },
+            label = { Text("Enter temperature in °Celsius") },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
 
         Spacer(modifier = Modifier.height(16.dp))
-
-        // Convert button
 
         Button(onClick = {
             val celsiusValue = inputText.toDoubleOrNull()
@@ -77,17 +71,16 @@ fun ConverterView() {
                 outputText = "Invalid input"
             }
         }) {
-            Text("Convert")
+            Text("Convert to Fahrenheit")
         }
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        // Output field
         Surface(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-                .background(Color.Gray),
+            color = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
         ) {
             Text(
                 text = outputText,
